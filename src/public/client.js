46b793d6b5725bdbde05cea5e8312d7f7bc2bbcd
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2021-04-01 23:41:01 +0100

    code review

--- src/public/client.js
+++ src/public/client.js
@@ -1,7 +1,7 @@
 let store = Immutable.Map({
     project: Immutable.Map({ name: "Mars Dashboard" }),
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-    roverName: ''
+    roverName: '',
 })
 
 // add our markup to the page
@@ -22,19 +22,18 @@ window.addEventListener('load', () => {
     getRoverData('Spirit')
     getRoverData('Opportunity')
     getRoverData('Curiosity')
-                 
 })
+
 
 const navMenu = () => {
     const navArray = store.get('rovers');
     return navArray.map(element => {
         return `<div class = rover>
-        <button id="${element}" href=# onclick="roverButton(${element})">${element}</button>
+        <button type="button" id="${element}" href=${element} onclick="roverButton(${element})">${element}</button>
         </div>
         `
     }).join(' ');//concatenating all of the elements in an array with space between and no coma
 }
-
 
 const Greeting = (name) => {
     if (name) {
@@ -47,37 +46,42 @@ const Greeting = (name) => {
         <h1>Hello!</h1>
     `
 }
-
 const renderRoverInfo = (state) => {
-    const roverData = state.photo_manifest;
+    const roverData = state.latest_photos;
     let roverDetails = ` 
-        <div id="roverInfo" style=display:none>
-            <p><b>Name:</b> ${roverData[0].rover.name}</p>
-            <p><b>Launch date:</b> ${roverData[0].rover.launch_date}
-            <p><b>Landing date:</b> ${roverData[0].rover.landing_date}</p>
-            <p><b>Status:</b> ${roverData[0].rover.status}
-            <p><b>Most recent photos taken on:</b> ${roverData.slice(-1).pop().earth_date}</p>
-        </div>
+        <div id="roverInfo">
+        <p><b>Name:</b> ${roverData[0].rover.name}</p>
+        <p><b>Launch date:</b> ${roverData[0].rover.launch_date}
+        <p><b>Landing date:</b> ${roverData[0].rover.landing_date}</p>
+        <p><b>Status:</b> ${roverData[0].rover.status}
+        <p><b>Most recent photos taken on:</b> ${roverData.slice(-1).pop().earth_date}</p>
+    </div>
         `
         return roverDetails;
 }
 
 const getRoverImage = (state) => {
     const roverData = state.latest_photos;
-    const sliceRoverData = roverData.slice(0, 6)
-    return sliceRoverData.map(e => {
-        return(`
-            <img src="${e.img_src}" style="width: 100%; height: auto" alt="Mars location"></img>
-`)
-    }).join(' ')
-}
+    console.log(roverData.length);
+        const roverDataSlice = roverData.slice(0, 4)
+        return roverDataSlice.map (e => {
+            console.log(e.img_src)
+            return(`
+            <div id='img-container'>
+                <img src="${e.img_src}"></img>
+            </div>
+            `)
+        }).join(' ')
+    }
+
+
 
 const renderRoverData = (state) => {
     return (
     `
     <header></header>
     <main>
-        <h1>${Greeting(store.get('project').get('name'))}</h1>
+            ${Greeting(store.get('project').get('name'))}
         <nav>
             ${navMenu()}
         </nav> 
@@ -90,23 +94,29 @@ const renderRoverData = (state) => {
         </main>
     `
     )       
-}
+    }
+
+
 
 function roverButton(button) {
     let element = document.getElementById('roverInfo');
-    if(element.style.display = 'none') {
-        element.style.display = 'block'
-    }
+
+    
     let selectedRover = button.id;
-    getRoverData(selectedRover)
+    console.log(selectedRover)
+    getRoverData(selectedRover)    
 }
 
+
 const getRoverData = (roverName) => { 
-    fetch(`http://localhost:3000/rover/${roverName}`)
+    fetch(`http://localhost:3005/rover/${roverName}`)
     .then(res => res.json())
     .then((roverData) => {
         let latest_photos = roverData.latest_photos
+        let photos = roverData.photos;
+        console.log(latest_photos) //shows "undefined"
         updateStore(store, {latest_photos});
-        render(root, store);
+        render(root, store)
     })
 }
+
