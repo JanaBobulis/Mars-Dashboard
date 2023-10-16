7432809eef5043569cdc821967f3491f50e0602c
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2023-10-16 16:41:26 +0100

    killing the theme

--- src/server/index.js
+++ src/server/index.js
@@ -13,13 +13,6 @@ app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  })
-
 // your API calls
 
 // example API call
